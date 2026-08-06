f2031a36f07e5f63e7f5489fd56a7b6506e72eed
TheRogueNico <theroguenico@gmail.com>
2026-08-06 23:01:57 +0330

    Normalize graph

--- main.go
+++ main.go
@@ -3,25 +3,35 @@ package main
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strings"
 )
 
 func createGraph(data map[rune]int) string {
-	graph := make([]string, 0)
-	character := ""
+	max := 0
+	for _, n := range data {
+		if n > max {
+			max = n
+		}
+	}
+
+	graph := make([]string, 0, len(data))
 	for k, n := range data {
+		var character string
 		switch k {
 		case '\n':
-			character = "'\\n'"
+			character = `'\n'`
 		case '\t':
-			character = "'\\t'"
+			character = `'\t'`
 		case ' ':
 			character = "' '"
 		default:
 			character = string(k)
 		}
-		bar := strings.Repeat("█", n)
+
+		barLen := int(math.Round(float64(n) / float64(max) * 40))
+		bar := strings.Repeat("█", barLen)
 		graph = append(graph, fmt.Sprintf("[%-4s:%3d] ▌%s", character, n, bar))
 	}
 	return strings.Join(graph, "\n")
